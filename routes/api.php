717c04d49f18d52785a0c87387b4bde9706171aa
Bimlendra <bimlendrayadav2075@gmail.com>
2023-10-08 17:39:12 +0545

    Logout Api Created

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,10 @@ Route::POST('/register',[AuthController::class, 'register']);
 Route::POST('/login',[AuthController::class, 'login']);
 
 
+Route::middleware(['auth:sanctum'])->group(function(){
+    Route::post('/logout',[AuthController::class, 'logout']);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
